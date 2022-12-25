d05bd55f75c2cdc4c3abf2f5b3bd7c5c3969f446
TheMegaSide <65494970+TheMegaSide@users.noreply.github.com>
2022-12-26 00:07:23 +0400

    Update DbExecutor.cs

--- SmokeWeb/Util/DB/DbExecutor.cs
+++ SmokeWeb/Util/DB/DbExecutor.cs
@@ -1,18 +1,25 @@
 ﻿using Npgsql;
 
 namespace SmokeWeb.Util.DB;
-
+//Класс, который выполняет соединение с базой данных и отправляет в нее запрос, полученный от BDService
 public class DbExecutor
 {
+    //Метод выполнения запроса
     public static List<T> Execute<T>(string connectionString, string query, IDbExecuteHandler<T> executeHandlerHandler)
     {
+        //Массив, который записывает полученные данные
         var returnObject = new List<T>();
+        //Создание экземпляра подключения
         using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
         {
+            //Открытие подключения
             con.Open();
+            //Создание экземпляра запроса
             using (NpgsqlCommand cmd = new NpgsqlCommand(query, con))
             {
+                //Получение данных через запрос
                 NpgsqlDataReader rdr = cmd.ExecuteReader();
+                //Запись полученных данных в массив returnObject
                 while (rdr.Read())
                 {
                     T item = executeHandlerHandler.GetDataAfterExecute(rdr);
@@ -20,7 +27,7 @@ public class DbExecutor
                 }
             }
         }
-
+        
         return returnObject;
     }
 }
